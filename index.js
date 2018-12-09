7ebe7234b1a37eca35fa6a053e5d73d16354d707
Jair Reina <Jair.Reina@digitaslbi.com>
2018-12-09 12:32:55 -0600

    adding colored version

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
 #!/usr/bin/env node
 const figlet = require('figlet');
-const inquirer = require('inquirer');
 const colors = require('colors/safe');
 
-const COLORS = [
-  'black',
-  'red',
-  'green',
-  'yellow',
-  'blue',
-  'magenta',
-  'cyan',
-  'white'
-];
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: "What's your name"
-    },
-    {
-      type: 'list',
-      name: 'color',
-      message: 'Choose a text color',
-      choices: COLORS,
-      default: 'white'
-    },
-    {
-      type: 'list',
-      name: 'bgColor',
-      message: 'Choose a background color',
-      choices: COLORS,
-      default: 'black'
+figlet('Jair  Reina', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
     }
 
-  ]).then(({name, color, bgColor})=>{
-    name = name.trim()==='' ? "C3PO":name;
-    bgColor = `bg${bgColor.charAt(0).toUpperCase() + bgColor.slice(1)}`;
+    log(data);
+    log('\t\t--------------');
+    log('\n');
+    log(`Hi! I'm Jair (a.k.a. @jaireina) Thanks for reaching out. \nLet's connect in any of my social media channels:`)
+    log('\n');
+    log(`  > LinkedIn: https://www.linkedin.com/in/jaireina/`);
+    log(`  > Instagram: https://www.instagram.com/jaireina/`);
+    log(`  > Twitter: https://twitter.com/jaireina`);
+    log(`  > Medium: https://medium.com/@jaireina`);
+    log(`  > Quora: https://www.quora.com/profile/Jair-Reina`)
+    log('\n');
+});
 
-    figlet(name, function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
-      }
-      console.log(colors[color][bgColor](data));
-    });
-  })
+function log(text){
+  console.log(colors.black.bgCyan(text));
+}
