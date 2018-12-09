1e46cb7791c1dd245f5db5e82204bdf4bc5c35a7
Jair Reina <Jair.Reina@digitaslbi.com>
2018-12-09 11:56:50 -0600

    adding colors and inquirer package and creating script to read users input and print its name with colors using figlet

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 #!/usr/bin/env node
 const figlet = require('figlet');
+const inquirer = require('inquirer');
+const colors = require('colors/safe');
 
-figlet('Jair  Reina', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
+const COLORS = [
+  'black',
+  'red',
+  'green',
+  'yellow',
+  'blue',
+  'magenta',
+  'cyan',
+  'white'
+];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What's your name"
+    },
+    {
+      type: 'list',
+      name: 'color',
+      message: 'Choose a text color',
+      choices: COLORS,
+      default: 'white'
+    },
+    {
+      type: 'list',
+      name: 'bgColor',
+      message: 'Choose a background color',
+      choices: COLORS,
+      default: 'black'
     }
-    console.log(data);
-    console.log('\t\t--------------');
-    console.log('\n');
-    console.log(`Hi! I'm Jair (a.k.a. @jaireina) Thanks for reaching out. \nLet's connect in any of my social media channels:`)
-    console.log('\n');
-    console.log(`  > LinkedIn: https://www.linkedin.com/in/jaireina/`);
-    console.log(`  > Instagram: https://www.instagram.com/jaireina/`);
-    console.log(`  > Twitter: https://twitter.com/jaireina`);
-    console.log(`  > Medium: https://medium.com/@jaireina`);
-    console.log(`  > Quora: https://www.quora.com/profile/Jair-Reina`)
-    console.log('\n');
-});
+
+  ]).then(({name, color, bgColor})=>{
+    name = name.trim()==='' ? "C3PO":name;
+    bgColor = `bg${bgColor.charAt(0).toUpperCase() + bgColor.slice(1)}`;
+
+    figlet(name, function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      console.log(colors[color][bgColor](data));
+    });
+  })
